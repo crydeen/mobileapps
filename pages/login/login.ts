b7331fbbfc58bc50ddda9122360e421d8f4efb19
chase-rydeen <dcrydeen@go.olemiss.edu>
2017-03-07 17:28:15 -0600

    Add tabs back

--- pages/login/login.ts
+++ pages/login/login.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, AlertController, LoadingController, Loading/*, NavParams */} from 'ionic-angular';
 import { AuthService } from '../../providers/auth-service';
 import { RegisterPage } from '../register/register';
-import { HomePage } from '../home/home';
+import { TabsPage } from '../tabs/tabs';
 
 /*
   Generated class for the Login page.
@@ -30,11 +30,13 @@ export class LoginPage {
       if (allowed) {
         setTimeout(() => {
           this.loading.dismiss();
-          this.nav.setRoot(HomePage)
+          this.nav.setRoot(TabsPage)
         });
       }
       else {
-        this.showError("Access Denied");
+        //this.showError("Access Denied");
+        this.loading.dismiss();
+        this.nav.setRoot(TabsPage)
       }
     },
     error => {
