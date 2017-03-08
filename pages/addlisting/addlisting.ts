e592b535b698d7a7593c4e07547995a7b36d459e
ljwa <leejacksonwalker@gmail.com>
2017-03-08 15:52:28 -0600

    Addlisting page and listings

---
+++ pages/addlisting/addlisting.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the Addlisting page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-addlisting',
+  templateUrl: 'addlisting.html'
+})
+export class AddlistingPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AddlistingPage');
+  }
+
+}
--- pages/listings/listings.ts
+++ pages/listings/listings.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Content } from 'ionic-angular';
 
 @Component({
   selector: 'page-listings',
   templateUrl: 'listings.html'
 })
+
+class E2EPage {
+  @ViewChild(Content) content: Content;
+
+  getItems() {
+    this.content.resize();
+  }
+}
 export class ListingsPage {
 
   constructor(public navCtrl: NavController) {
